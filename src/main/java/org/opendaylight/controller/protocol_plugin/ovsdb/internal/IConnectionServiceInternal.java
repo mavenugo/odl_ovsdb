b9e9e6df5d2dce80676ba038c6cbef814d4e3060
Madhu Venugopal <vmadhu@cisco.com>
2013-07-21 13:34:32 -0700

    Removed old files

--- src/main/java/org/opendaylight/controller/protocol_plugin/ovsdb/internal/IConnectionServiceInternal.java
+++
@@ -1,7 +0,0 @@
-package org.opendaylight.controller.protocol_plugin.ovsdb.internal;
-
-import org.opendaylight.controller.sal.core.Node;
-
-public interface IConnectionServiceInternal {
-    public Connection getConnection(Node node);
-}
